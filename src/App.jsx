32d82f574b4f8b63b1467392fb98594c785c0163
ThanhbknAI <nhatthanh162.fpt@gmail.com>
2024-05-11 00:18:45 +0700

    feat: Add search bar functionality and prepare images and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
+
 import './App.css'
 
+import search from './assets/icons/search.svg'
+
 function App() {
+
+  const [input ,setInput] = useState('')
+
   return (
-    <div className='text-xl font-bold text-center'> 
-      Hello World
+    <div className='w-full h-screen text-white px-8'>
+
+      <nav className='w-full p-3 flex justify between items-center'>
+        
+       <h1 className='font-bold tracking-wide text-3x1'>Weather App</h1> 
+        
+        <div className='bg-white w-[15rem] overflow-hidden shadow-2x1 rounded flex items-center p-2 gap-2'>
+        
+            <img src={search} alt="Search" />
+        
+            <input onKeyU = {(e) => {
+              if (e.key === 'Enter'){
+                // summit form
+              }
+            }} type="text" className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+        
+        </div>
+      
+      </nav>
 
     </div>
   )
